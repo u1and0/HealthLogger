15f0dc1b48f2e8a618f5b38d275843e60f78da5d
u1and0 <e01.ando60@gmail.com>
2022-12-22 21:06:24 +0900

    initial commit

---
+++ check.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python3
+"""DAQ970シリーズをリモート操作します。
+毎秒CHANに指定したチャンネルを測定します。
+測定値がLIMITを下回ると ビープ音を鳴らして、画面を暗転します。
+
+Toggle led(GPIO17) HIGH / LOW by push switch(GPIO18)
+
+,--------------------------------.
+| oooooooooooooooooooo J8   +======
+| 1ooooooooooooooooooo  PoE |   Net
+|  Wi                    1o +======
+|  Fi  Pi Model 4B  V1.5 oo      |
+|        ,----. +---+         +====
+| |D|    |SoC | |RAM|         |USB3
+| |S|    |    | |   |         +====
+| |I|    `----' +---+            |
+|                   |C|       +====
+|                   |S|       |USB2
+| pwr   |hd|   |hd| |I||A|    +====
+`-| |---|m0|---|m1|----|V|-------'
+
+J8:
+   3V3  (1) (2)  5V
+ GPIO2  (3) (4)  5V
+ GPIO3  (5) (6)  GND
+ GPIO4  (7) (8)  GPIO14
+   GND  (9) (10) GPIO15
+    `--------------------------------`
+GPIO17 (11) (12) GPIO18  -- SWITCH --|
+    `---------------------- LED    --'
+GPIO27 (13) (14) GND
+GPIO22 (15) (16) GPIO23
+   3V3 (17) (18) GPIO24
+GPIO10 (19) (20) GND
+ GPIO9 (21) (22) GPIO25
+GPIO11 (23) (24) GPIO8
+   GND (25) (26) GPIO7
+ GPIO0 (27) (28) GPIO1
+ GPIO5 (29) (30) GND
+ GPIO6 (31) (32) GPIO12
+GPIO13 (33) (34) GND
+GPIO19 (35) (36) GPIO16
+GPIO26 (37) (38) GPIO20
+   GND (39) (40) GPIO21
+
+POE:
+TR01 (1) (2) TR00
+TR03 (3) (4) TR02
+
+For further information, please refer to https://pinout.xyz/
+"""
+import sys
+import time
+import logging
+import RPi.GPIO as GPIO
+import pydaq
+
+# Pin setting
+led_pin = 17
+switch_pin = 18
+# GPIO setting
+GPIO.setmode(GPIO.BCM)
+# set IN mode and enable pull up
+GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(led_pin, GPIO.OUT)  # set OUT mode
+GPIO.output(led_pin, GPIO.LOW)  # default OFF
+
+# for closure
+pushed_time = None
+
+
+# Measure Option
+LIMIT = 1.1e3
+WARNING = 1.8e3
+CHAN = "101,120"
+# chanlist1 = "101:113"
+# chanlist2 = "201:213"
+
+
+def blink():
+    """blink 5 times per 0.5 sec"""
+    for _ in range(5):
+        GPIO.output(led_pin, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(led_pin, GPIO.LOW)
+        time.sleep(0.5)
+
+
+class CustomFormatter(logging.Formatter):
+    """[WARNING] -> [WARN] のように表示を先頭4文字に変更するカスタムフォーマッタ"""
+
+    def format(self, record):
+        record.levelname = record.levelname[:4]
+        return super().format(record)
+
+
+# Logging option
+# Set log level
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Set stream handler
+stream_handler = logging.FileHandler("output.log", "w", "utf-8")
+stream_handler.setLevel(logging.DEBUG)
+
+# Set file handler
+file_handler = logging.StreamHandler()
+file_handler .setLevel(logging.DEBUG)
+
+# Set log format
+formatter = CustomFormatter("[ %(levelname)s ] %(asctime)s: %(message)s")
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# Add handlers to logger
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+# Initialize instruments
+try:
+    daq = pydaq.Daq()
+except BaseException as e:
+    logger.critical(e)
+    sys.exit(1)
+
+try:
+    while True:
+        #
+        # vol1 = daq.measure("MEAS:VOL? (@120)")
+        #
+        command = (
+            # Measure resistance
+            f"CONF:RES 10E6,10, (@{CHAN})",
+            # 10V以上の電圧があればスイッチが入っている(@120)で測らない
+            # "CONF:RES 10E6,10, (@{})".format(chanlist2 if vol1>10 else chanlist1)
+            "RES:NPLC 1",
+
+            # Set alert state
+            # Display Red font
+            f"CALC:LIMIT:LOW {WARNING}",
+            "CALC:LIMIT:LOW:STATE ON",
+        )
+        res = daq.measure(*command)
+        res_csv = ",".join(str(i) for i in res)
+
+        # resの返り値の内１つでもLIMITを下回ったら
+        if any(r < LIMIT for r in res):
+            # Beep と画面暗転
+            daq.write("SYSTEM:BEEP")
+            daq.write("DISP:TEXT '[ CAUTION ]\nSHUTDOWN THE SYSTEM'")
+            logger.error(res_csv)
+        # resの返り値の内１つでもWARNINGを下回ったら
+        elif any(r < WARNING for r in res):
+            # warning, info levelのときは画面暗転を解除
+            daq.write("DISP:TEXT:CLEAR")
+            logger.warning(res_csv)
+        else:
+            # warning, info levelのときは画面暗転を解除
+            daq.write("DISP:TEXT:CLEAR")
+            logger.info(res_csv)
+
+        # ボタンを押した瞬間の検出
+        if GPIO.input(switch_pin) == GPIO.LOW and pushed_time is None:
+            pushed_time = time.time()
+
+        # ボタンを離した瞬間の検出
+        if GPIO.input(switch_pin) == GPIO.HIGH and pushed_time is not None:
+            button_duration = time.time() - pushed_time
+
+            if button_duration < 1.0:
+                logger.warning("Receiving reboot signal.")
+                blink()  # 短押しで点滅
+                # subprocess.run(["reboot"])
+            else:
+                logger.warning("Receiving shutdown signal.")
+                GPIO.output(led_pin, GPIO.HIGH)  # 長押しで点灯
+                # subprocess.run(["shutdown","-h","0"])
+
+            # スイッチを離したらタイムスタンプをリセット
+            pushed_time = None
+
+        # 毎秒測定
+        time.sleep(1)
+
+finally:
+    # DAQ session handler close
+    daq.write("STATUS:PRESET")
+    daq.write("DISP:TEXT:CLEAR")
+    daq.write("*CLS")
+    daq.close()
+    # GPIO開放
+    GPIO.cleanup()
---
+++ pydaq.py
@@ -0,0 +1,229 @@
+"""
+Keysight 測定器を操作する
+
+Keysight測定器のプログラムリファレンス
+https://www.batronix.com/files/Keysight/DAQ/DAQ970A-973A-Programming.pdf
+
+case insensitive なので upper caseでも lower caseでもコマンドを受け付けるが、
+このコードではupper caseで統一して記述します。
+"""
+import time
+from typing import Union
+from enum import Enum
+import pyvisa
+
+
+class Range(Enum):
+    AUTO = "AUTO"
+    MIN = "MIN"
+    MAX = "MIN"
+    DEF = "DEF"
+
+    def __str__(self):
+        return self.name.upper()
+
+
+class Resolution(Enum):
+    MIN = "MIN"
+    MAX = "MAX"
+    DEF = "DEF"
+
+    def __str__(self):
+        return self.name.upper()
+
+
+class Daq:
+    """ Keysight DAQ970A/DAQ973A controller """
+
+    def __init__(self):
+        """ リソースの開放
+        ポートをスキャンして
+        USB から始まるポートに割当て。
+
+        インタープリターでやっているときはcloseしないと
+        他のセッションからアクセスできなくなるので注意
+
+        Usage:
+            >>> daq = Daq()
+            Check resources...
+            ASRL/dev/ttyAMA0::INSTR
+            USB0::10893::34305::MY59002752::0::INSTR
+            initialize success.
+            >>> daq.whoami
+            'Keysight Technologies,DAQ973A,MY59002752,A.02.02-01.00-02.01-00.02-02.00-03-03\n'
+
+        open_resource()で作成してインスタンスへアクセスするには
+        daq.instr.{method}を使ってください。
+        >>> daq.instr.write("MEAS:VOLT:DC? AUTO,DEF, (@110)")
+        32
+        >>> daq.instr.read()
+        '+3.29713452E+00\n'
+
+        """
+        # Begin finding out the daq resources that are available.
+        self.rm = pyvisa.ResourceManager()
+        # Make a variable that is the list of visa devices attached to the computer.
+        resources = self.rm.list_resources()
+        print("Check resources...")
+
+        for r in resources:
+            print(r)
+            if r.startswith("USB"):
+                self.instr = self.rm.open_resource(r)
+                print("initialize success.")
+                break
+
+        # 型式を取得
+        # 'Keysight Technologies,DAQ973A,MY59002752,A.02.02-01.00-02.01-00.02-02.00-03-03\n'
+        self.whoami = self.hello()
+
+    def hello(self):
+        return self.instr.query("*IDN?")
+
+    def close(self):
+        """ 他のセッションからアクセスするためにはリソースを閉じなければならない """
+        self.rm.close()
+
+    def __del__(self):
+        self.close()
+
+    @staticmethod
+    def parse_string(*ch: Union[str, int]) -> str:
+        """
+        101,102,... のようにカンマで結合した文字列に変換
+        >>> Daq.parse_string(101, 102)
+        "101,102"
+        """
+        # :が含まれていたら 101:110 のようなリスト形式で指定されたものとする
+        if ":" in (sch := "".join(str(c) for c in ch)):
+            return sch
+        return ",".join(str(c) for c in ch)
+
+    @staticmethod
+    def parse_float(st: str) -> Union[float, list[float]]:
+        """
+        '+1.99674538E+03,+2.63265505E+04' という文字列をカンマで区切って
+        floatとして解釈し floatのリストで返す
+        >>> Daq.parse_float('+1.99674538E+03,+2.63265505E+04' )
+        [1996.74,26326.55]
+        """
+        # rstrip()で\nを削除して,で区切ってfloatリストにする
+        lst = [float(r) for r in st.rstrip().split(",")]
+        if len(lst) < 2:
+            return lst[0]
+        return lst
+
+    def resistance(
+        self,
+        *ch: Union[int, str],
+        range_: Union[Range, int] = Range.AUTO,
+        resolution: Union[Resolution, int] = Resolution.DEF
+    ) -> Union[float, list[float]]:
+        """ 抵抗測定
+        # 1の基板の01の抵抗を測定
+        >>> daq.resistance(101)
+
+        # 2の基板の01と03の抵抗を測定
+        >>> daq.resistance(201, 203)
+
+        # 1の基板の01から03の抵抗を測定
+        >>> daq.resistance("101:103")
+        chにはチャネル番号、基板1なら101～120まで、基板2なら201～220まで。
+
+        NOTE: Keysightの仕様により、channel listは昇順ソートされる。
+        よって、以下のように降順やバラバラの順番にchannelを渡しても
+        ソートされるので、返ってくる値は同じ
+
+        >>> daq.resistance(102,101)
+        [1996.75952, 99.39895]
+
+        >>> daq.resistance(101,102)
+        [1996.76062, 99.3987556]
+        """
+        chs = Daq.parse_string(*ch)
+        res = self.instr.query(
+            f"MEAS:RES? {range_},{resolution}, (@{chs})")
+        ary = Daq.parse_float(res)
+        return ary
+
+    def voltage(
+        self,
+        *ch: Union[int, str],
+        range_: Union[Range, int] = Range.AUTO,
+        resolution: Union[Resolution, int] = Resolution.DEF
+    ) -> Union[float, list[float]]:
+        """ 抵抗測定
+        # 1の基板の01の抵抗を測定
+        >>> daq.resistance(101)
+
+        # 2の基板の01と03の抵抗を測定
+        >>> daq.resistance(201, 203)
+
+        chにはチャネル番号、基板1なら101～120まで、基板2なら201～220まで。
+        """
+        chs = Daq.parse_string(*ch)
+        res = self.instr.query(
+            f"MEAS:VOLT:DC? {range_},{resolution}, (@{chs})")
+        return Daq.parse_float(res)
+
+    def measure(self, *message: str, delay: float = 0.0, **kwargs):
+        """queryの結果をfloatにパースする"""
+        res = self.query(*message, delay=delay, **kwargs)
+        return Daq.parse_float(res)
+
+    def query(self, *message: str, delay: float = 0.0, **kwargs):
+        """Send any command to DAQ
+
+        ex)
+        Channel 101の抵抗値をrange:Auto, resolution: Defaultで測定する
+        >>> daq.query("MEAS:RES? AUTO,DEF, (@101)")
+        '+1.99672270E+03\n'
+
+        RES?のあとにスペース入れないとエラー
+
+        Returns will be raw string included newline "\n".
+        """
+        # Write all messages one by one
+        for command in message:
+            self.instr.write(command, **kwargs)
+
+        # Wait for command processing
+        if delay > 0.0:
+            time.sleep(delay)
+
+        # Finally read buffer from instrument
+        self.instr.write("READ?")
+        return self.instr.read()
+
+    def write(self, *args, **kwargs):
+        """ Same as daq.instr.write()
+        >>> daq.write("MEAS:VOLT:DC? AUTO,DEF, (@110)")
+
+        誤情報。
+        ~~`daq.write()`を複数行入れるときは一つの関数に入れられる~~
+        >>> daq.write("MEAS:VOLT:DC? AUTO,DEF, (@110)", "read?")
+
+        positional引数は1つしか受け付けない。
+        >>> daq.write("MEAS:VOLT:DC? AUTO,DEF, (@110)")
+        >>> daq.write("read?")
+        >>> daq.read()
+
+        代わりに`\n`で区切れば(DAQ973Aの場合は？)複数コマンド入れられる。
+
+        >>> daq.write("MEAS:VOLT:DC? AUTO,DEF, (@110)\nread?")
+        >>> daq.read()
+
+        write(), read()を打つのが冗長なので、query()を打つことがほとんど。
+        query()は内部でwrite, read()を行っている。
+        """
+        return self.instr.write(*args, **kwargs)
+
+    def read(self, *args, **kwargs):
+        """ Same as daq.instr.read()
+        write()したバッファを読み込む。
+
+        >>> response = daq.read()
+        >>> Daq.parse_float(response)
+        3.3245
+        """
+        return self.instr.read(*args, **kwargs)
